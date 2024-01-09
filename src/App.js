08f981d27668de055069bd9619e55fcbcbfa9f14
Garbi93 <iseung2093@gmail.com>
2024-01-09 16:37:02 +0900

    라우터 셋팅

--- src/App.js
+++ src/App.js
@@ -7,7 +7,27 @@ import {
 import { HomeLayout } from "./layout/HomeLayout";
 
 const routes = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<HomeLayout />}></Route>),
+  createRoutesFromElements(
+    <Route path="/" element={<HomeLayout />}>
+      {/* 메인페이지 */}
+
+      {/* 상품관련 */}
+
+      {/* 리뷰 */}
+
+      {/* 장바구니 */}
+
+      {/* 찜하기 */}
+
+      {/* Q&A */}
+
+      {/* 결제 */}
+
+      {/* 회원관련 */}
+
+      {/* 게시판관련 */}
+    </Route>,
+  ),
 );
 
 function App() {
