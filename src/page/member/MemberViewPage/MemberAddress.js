4b0a159efb55d08596ffe483a77633f1593c0cab
Garbi93 <iseung2093@gmail.com>
2024-01-15 17:45:12 +0900

    01/15 주소 수정 페이지 작성

--- src/page/member/MemberViewPage/MemberAddress.js
+++ src/page/member/MemberViewPage/MemberAddress.js
@@ -7,6 +7,9 @@ import {
   CardHeader,
   Center,
   Flex,
+  FormControl,
+  FormLabel,
+  Input,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -14,6 +17,7 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Switch,
   Table,
   Tbody,
   Td,
@@ -26,14 +30,17 @@ import {
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { useDaumPostcodePopup } from "react-daum-postcode";
 
 export function MemberAddress() {
   const [memberAddress, setMemberAddress] = useState([]);
   const [member, setMember] = useState("");
 
+  // 선택한 주소의 정보
   const [selectedAddress, setSelectedAddress] = useState("");
 
-  // 주소록 상태 변경 인식 코드 -----------------------------------------------------
+  // 주소록 주소가 삭제 되었는지 인식 코드 -----------------------------------------------------
+  // 첫 랜더링 시 false 상태
   const [addressState, setAddressState] = useState(false);
 
   const toast = useToast();
@@ -53,14 +60,14 @@ export function MemberAddress() {
   } = useDisclosure();
 
   // 수정 모달 열기 -----------------------------------------------------------------
-  const openEditModal = () => {
+  const openEditModal = (address) => {
+    setSelectedAddress(address);
     onEditOpen();
   };
 
   // 삭제 모달 열기 -----------------------------------------------------------------
   const openDeleteModal = (address) => {
     setSelectedAddress(address);
-    console.log(address);
     onDeleteOpen();
   };
 
@@ -89,7 +96,7 @@ export function MemberAddress() {
     });
   }, []);
 
-  // 삭제 버튼 클릭 -----------------------------------------------------------------
+  // 삭제 모달의 삭제 버튼 클릭 -----------------------------------------------------------------
   function handleDeleteClick() {
     if (selectedAddress.member_address_type === "main") {
       toast({
@@ -117,6 +124,79 @@ export function MemberAddress() {
           });
         });
     }
+  }
+
+  function handleMainAddressSwitch(e) {
+    if (e.target.checked) {
+      setSelectedAddress({ ...selectedAddress, member_address_type: "main" });
+    } else {
+      setSelectedAddress({ ...selectedAddress, member_address_type: "sub" });
+    }
+  }
+
+  // Daum Postcode 스크립트 URL
+  const scriptUrl =
+    "https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js";
+  // Daum Postcode 팝업을 여는 함수
+  const openPostcodePopup = useDaumPostcodePopup(scriptUrl);
+  // 주소 검색 완료 핸들러
+  const handleComplete = (data) => {
+    let fullAddress = data.address;
+    let extraAddress = "";
+
+    if (data.addressType === "R") {
+      if (data.bname !== "") {
+        extraAddress += data.bname;
+      }
+      if (data.buildingName !== "") {
+        extraAddress +=
+          extraAddress !== "" ? `, ${data.buildingName}` : data.buildingName;
+      }
+      fullAddress += extraAddress !== "" ? ` (${extraAddress})` : "";
+    }
+
+    setSelectedAddress({
+      ...selectedAddress,
+      member_address: fullAddress,
+      member_post_code: data.zonecode,
+      member_detail_address: "",
+    });
+  };
+
+  // 수정 모달에서 주소검색 클릭 ----------------------------------------------------------------
+  function handlePostCodeClick() {
+    openPostcodePopup({ onComplete: handleComplete });
+  }
+
+  // 수정 모달 에서 수정 버튼 클릭 ---------------------------------------------------------------
+  function handleEditAddressClick() {
+    axios
+      .put("/api/memberAddress/editAddress", {
+        id: selectedAddress.id,
+        member_address: selectedAddress.member_address,
+        member_address_name: selectedAddress.member_address_name,
+        member_address_type: selectedAddress.member_address_type,
+        member_detail_address: selectedAddress.member_detail_address,
+        member_post_code: selectedAddress.member_post_code,
+      })
+      .then(() => {
+        toast({ description: "주소가 수정 되었습니다.", status: "success" });
+        setAddressState((prev) => !prev);
+      })
+      .catch((error) => {
+        if (error) {
+          toast({
+            description: error.response.data,
+            status: "error",
+          });
+        } else {
+          toast({
+            description: "수정중 문제가 발생 하였습니다.",
+            status: "error",
+          });
+        }
+      })
+      .finally(onEditClose);
   }
 
   return (
@@ -163,7 +243,9 @@ export function MemberAddress() {
                     <Tr>
                       <Td colSpan={5}>
                         <Flex gap={2} mt={2}>
-                          <Button onClick={openEditModal}>수정</Button>
+                          <Button onClick={() => openEditModal(address)}>
+                            수정
+                          </Button>
                           <Button onClick={() => openDeleteModal(address)}>
                             삭제
                           </Button>
@@ -190,24 +272,152 @@ export function MemberAddress() {
           onClose={onEditClose}
           isOpen={isEditOpen}
           motionPreset="slideInBottom"
+          size={"xl"}
         >
           <ModalOverlay />
           <ModalContent>
             <ModalHeader>주소 수정</ModalHeader>
             <ModalCloseButton />
-            <ModalBody>수정 페이지 입니다.</ModalBody>
+            <ModalBody>
+              <CardBody>
+                {/* 주소 별명 */}
+                <FormControl mt={2}>
+                  <Flex justifyContent={"center"}>
+                    <FormLabel
+                      w={"100px"}
+                      fontSize={"1.1rem"}
+                      lineHeight={"50px"}
+                    >
+                      주소 별명
+                    </FormLabel>
+                    <Input
+                      w={"500px"}
+                      h={"50px"}
+                      borderRadius={"0"}
+                      placeholder={"주소 별명을 입력해 주세요."}
+                      value={selectedAddress.member_address_name}
+                      onChange={(e) =>
+                        setSelectedAddress({
+                          ...selectedAddress,
+                          member_address_name: e.target.value,
+                        })
+                      }
+                    />
+                  </Flex>
+                </FormControl>
+                {/* 우편번호 */}
+                <FormControl mt={2}>
+                  <Flex justifyContent={"center"}>
+                    <FormLabel
+                      w={"100px"}
+                      fontSize={"1.1rem"}
+                      lineHeight={"50px"}
+                    >
+                      우편번호
+                    </FormLabel>
+                    <Input
+                      placeholder={"주소 검색을 클릭 해주세요."}
+                      w={"350px"}
+                      h={"50px"}
+                      borderRadius={"0"}
+                      readOnly
+                      value={selectedAddress.member_post_code}
+                    />
+                    <Button
+                      w={"140px"}
+                      h={"50px"}
+                      ml={"10px"}
+                      onClick={handlePostCodeClick}
+                    >
+                      주소검색
+                    </Button>
+                  </Flex>
+                </FormControl>
+                {/* 주소 */}
+                <FormControl mt={2}>
+                  <Flex justifyContent={"center"}>
+                    <FormLabel
+                      w={"100px"}
+                      fontSize={"1.1rem"}
+                      lineHeight={"50px"}
+                    >
+                      주소
+                    </FormLabel>
+                    <Input
+                      w={"500px"}
+                      h={"50px"}
+                      borderRadius={"0"}
+                      readOnly
+                      value={selectedAddress.member_address}
+                    />
+                  </Flex>
+                </FormControl>
+                {/* 상세주소 */}
+                <FormControl mt={2}>
+                  <Flex justifyContent={"center"}>
+                    <FormLabel
+                      w={"100px"}
+                      fontSize={"1.1rem"}
+                      lineHeight={"50px"}
+                    >
+                      상세주소
+                    </FormLabel>
+                    <Input
+                      placeholder={"상세주소를 입력해 주세요"}
+                      w={"500px"}
+                      h={"50px"}
+                      borderRadius={"0"}
+                      value={selectedAddress.member_detail_address}
+                      onChange={(e) =>
+                        setSelectedAddress({
+                          ...selectedAddress,
+                          member_detail_address: e.target.value,
+                        })
+                      }
+                    />
+                  </Flex>
+                </FormControl>
+                <FormControl mt={2}>
+                  <Flex align="center" justify="center">
+                    <FormLabel
+                      w={"130px"}
+                      fontSize={"1.1rem"}
+                      lineHeight={"50px"}
+                    >
+                      기본 주소 설정
+                    </FormLabel>
+                    <Box
+                      w={"470px"}
+                      h={"50px"}
+                      display={"flex"}
+                      alignItems={"center"}
+                    >
+                      <Switch
+                        size="lg"
+                        colorScheme={"gray"}
+                        isChecked={
+                          selectedAddress.member_address_type === "main"
+                        }
+                        onChange={handleMainAddressSwitch}
+                      />
+                    </Box>
+                  </Flex>
+                </FormControl>
+              </CardBody>
+            </ModalBody>
             <ModalFooter>
               <Button
-                colorScheme="red"
                 mr={3}
-                onClick={() => {
-                  // 여기에 수정 로직을 추가하세요
-                  onEditClose();
-                }}
+                onClick={handleEditAddressClick}
+                style={{ backgroundColor: "black", color: "whitesmoke" }}
               >
                 수정
               </Button>
-              <Button variant="ghost" onClick={onEditClose}>
+              <Button
+                colorScheme={"gray"}
+                border={"1px solid whitesmoke"}
+                onClick={onEditClose}
+              >
                 취소
               </Button>
             </ModalFooter>
