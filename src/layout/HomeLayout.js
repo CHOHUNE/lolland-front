d7570f9ac2343c461b08bb4334d39ed464c22d90
LeeJungHoon <gns14585@naver.com>
2024-01-16 17:37:41 +0900

    gns14585 찜하기 완료

--- src/layout/HomeLayout.js
+++ src/layout/HomeLayout.js
@@ -2,10 +2,15 @@ import { Box } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import { NavBar } from "../component/NavBar";
 import { Footer } from "../component/Footer";
+import "../global.css";
 
 export function HomeLayout() {
   return (
-    <Box>
+    <Box
+      style={{
+        fontFamily: "Pretendard-Regular",
+      }}
+    >
       <NavBar fontWeight={"700"} />
       <Outlet />
       <Footer fontWeight={"700"} />
