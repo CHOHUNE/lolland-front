f005488e91449e089c05b80e2d18d4e89b0349be
Garbi93 <iseung2093@gmail.com>
2024-01-19 15:20:44 +0900

    01/19 어드민이 회원 탈퇴 클릭시 탈퇴 모달 띄우기

--- src/page/admin/MemberList.js
+++ src/page/admin/MemberList.js
@@ -1,18 +1,60 @@
 import {
+  Box,
   Button,
   Card,
   CardBody,
   CardHeader,
   Center,
+  Flex,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Table,
   Tbody,
   Td,
   Th,
   Thead,
   Tr,
+  useDisclosure,
+  useToast,
 } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
 export function MemberList() {
+  const [memberList, setMemberList] = useState([]);
+  const [selectMember, setSelectMember] = useState("");
+
+  const toast = useToast();
+
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
+  useEffect(() => {
+    axios
+      .get("/api/member/listAll")
+      .then((response) => {
+        setMemberList(response.data);
+      })
+      .catch(() => {
+        toast({
+          description: "회원 목록 조회에 실패 했습니다.",
+          status: "error",
+        });
+      });
+  }, []);
+
+  // 삭제 버튼 클릭시 동작
+  const handleMemberDeleteClick = (e) => {
+    setSelectMember(e);
+    onOpen();
+  };
+
   return (
     <Center>
       <Card>
@@ -42,19 +84,60 @@ export function MemberList() {
               </Tr>
             </Thead>
             <Tbody>
-              <Tr>
-                <Td textAlign={"center"}>user</Td>
-                <Td textAlign={"center"}>유저</Td>
-                <Td textAlign={"center"}>010-1111-2222</Td>
-                <Td textAlign={"center"}>user@gmail.com</Td>
-                <Td textAlign={"center"}>2024년 01월 19일</Td>
-                <Td textAlign={"center"}>
-                  <Button colorScheme={"red"}>탈퇴</Button>
-                </Td>
-              </Tr>
+              {memberList.map((member) => (
+                <Tr key={member.id}>
+                  <Td textAlign={"center"}>{member.member_login_id}</Td>
+                  <Td textAlign={"center"}>{member.member_name}</Td>
+                  <Td textAlign={"center"}>{member.member_phone_number}</Td>
+                  <Td textAlign={"center"}>{member.member_email}</Td>
+                  <Td textAlign={"center"}>{member.reg_time}</Td>
+                  <Td textAlign={"center"}>
+                    <Button
+                      colorScheme={"yellow"}
+                      onClick={() => handleMemberDeleteClick(member)}
+                    >
+                      탈퇴
+                    </Button>
+                  </Td>
+                </Tr>
+              ))}
             </Tbody>
           </Table>
         </CardBody>
+
+        {/* 탈퇴 모달 */}
+        <>
+          <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent bg={"black"} color={"white"}>
+              <ModalHeader>회원 탈퇴 처리</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <Flex mb={2}>
+                  <Box
+                    mr={2}
+                    fontSize={"1.2rem"}
+                    fontWeight={"900"}
+                    color={"yellowgreen"}
+                  >
+                    {selectMember.member_login_id}
+                  </Box>
+                  <Box>님을 탈퇴 처리 합니다.</Box>
+                </Flex>
+                <Box>탈퇴 처리후 복구 불가능 합니다.</Box>
+              </ModalBody>
+
+              <ModalFooter>
+                <Button mr={3} onClick={onClose}>
+                  Close
+                </Button>
+                <Button colorScheme={"red"}>
+                  <FontAwesomeIcon icon={faTrashCan} />
+                </Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        </>
       </Card>
     </Center>
   );
