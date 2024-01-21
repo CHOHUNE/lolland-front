6ab1f0cc7958cdbbf424313f37d55dbb2ee60856
이정훈 <gns14585@naver.com>
2024-01-22 00:43:00 +0900

    gns14585 ProductStats : 별점 분포도 표시 (useEffect 활용해서 상품 렌더링 될 때 average_rate로 평점 표시
    RatingChart : 기존 코드로 원상복귀

--- src/page/review/RatingChart.js
+++ src/page/review/RatingChart.js
@@ -2,21 +2,32 @@ import React, { useRef, useEffect } from "react";
 import { Chart, Tooltip, TooltipItem } from "chart.js/auto";
 import { Bar } from "react-chartjs-2";
 
-const RatingChart = ({ ratingDistribution }) => {
-  const labels = [0, 1, 2, 3, 4, 5];
-  const data = labels.map((label) =>
-    ratingDistribution[label] !== undefined ? ratingDistribution[label] : 0,
-  );
+const RatingChart = ({ ratingDistribution, boxDimensions }) => {
+  const chartRef = useRef(null);
 
-  const chartData = {
-    labels: labels.map(String),
-    datasets: [
-      {
-        data: data,
-        backgroundColor: 'orange',
-      },
-    ],
-  };
+  useEffect(() => {
+    if (chartRef.current) {
+      chartRef.current.destroy();
+    }
+
+    const labels = [0, 1, 2, 3, 4, 5];
+    const data = labels.map((label) =>
+      ratingDistribution[label] !== undefined ? ratingDistribution[label] : 0,
+    );
+
+    const chartContainer = document.getElementById("rating-chart");
+    chartContainer.width = boxDimensions.width;
+    chartContainer.height = boxDimensions.height;
+
+    const chartData = {
+      labels: labels.map(Number),
+      datasets: [
+        {
+          data: data,
+          backgroundColor: "orange",
+        },
+      ],
+    };
 
     const options = {
       scales: {
@@ -71,7 +82,18 @@ const RatingChart = ({ ratingDistribution }) => {
       },
     };
 
-  return <Bar data={chartData} options={options} />;
+    chartRef.current = new Chart(document.getElementById("rating-chart"), {
+      type: "bar",
+      data: chartData,
+      options: options,
+    });
+
+    return () => {
+      chartRef.current.destroy();
+    };
+  }, [ratingDistribution, boxDimensions]);
+
+  return <canvas id="rating-chart" />;
 };
 
 export default RatingChart;
