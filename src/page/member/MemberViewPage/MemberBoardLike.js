ec047619bb2b65024f6259a23a697768d82b8fa7
Garbi93 <iseung2093@gmail.com>
2024-01-24 16:43:06 +0900

    01/24 회원 좋아요 전체 선택 전체 삭제 선택한것들만 삭제 되고 한개만 삭제도 가능하게 배열로 값 전달

--- src/page/member/MemberViewPage/MemberBoardLike.js
+++ src/page/member/MemberViewPage/MemberBoardLike.js
@@ -19,8 +19,10 @@ import {
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faRectangleXmark } from "@fortawesome/free-regular-svg-icons";
+
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faThumbsUp } from "@fortawesome/free-regular-svg-icons";
+import { useNavigate } from "react-router-dom";
 
 export function MemberBoardLike() {
   // 버튼 css
@@ -43,7 +45,12 @@ export function MemberBoardLike() {
   // 좋아요 삭제 인식
   const [deletedLikeStatus, setDeletedLikeStatus] = useState(false);
 
+  // 체크 박스 에서 선택한 게임 게시글
+  const [checkLikeGameBoard, setCheckLikeGameBoard] = useState([]);
+
   const toast = useToast();
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios.get("/api/member/getGameBoardLike").then((response) => {
@@ -55,9 +62,10 @@ export function MemberBoardLike() {
   const handleLikeDeleteClick = (gameBoardId) => {
     axios
       .delete("/api/member/deleteGameBoardLike", {
-        params: {
-          gameBoardId,
-        },
+        data: [gameBoardId],
+        // headers: {
+        //   "Content-Type": "application/json",
+        // },
       })
       .then(() => {
         setDeletedLikeStatus((prev) => !prev);
@@ -70,6 +78,45 @@ export function MemberBoardLike() {
       });
   };
 
+  // 좋아요 게시글의 체크
+  const handleLikeRowChange = (e, gameBoardId) => {
+    if (e.target.checked === true) {
+      setCheckLikeGameBoard((prev) => [...prev, gameBoardId]);
+    } else {
+      setCheckLikeGameBoard((prev) => prev.filter((id) => id !== gameBoardId));
+    }
+  };
+
+  // 전체 선택 버튼 클릭시
+  const handleSelectAll = () => {
+    setCheckLikeGameBoard(gameBoardList.map((gameBoard) => gameBoard.id));
+  };
+  // 전체 해제 버튼 클릭시
+  const handleDesSelectAll = () => {
+    setCheckLikeGameBoard([]);
+  };
+
+  // 선택 삭제 클릭시 선택 한것들만 지우기
+  function handleSelectDeleteClick() {
+    console.log(checkLikeGameBoard);
+    axios
+      .delete("/api/member/deleteGameBoardLike", {
+        data: checkLikeGameBoard,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then(() => {
+        setDeletedLikeStatus((prev) => !prev);
+      })
+      .catch(() => {
+        toast({
+          description: "좋아요 삭제중 문제가 발생했습니다.",
+          status: "error",
+        });
+      });
+  }
+
   return (
     <Center>
       <Card shadow={"1px 1px 3px 1px #dadce0"}>
@@ -80,7 +127,40 @@ export function MemberBoardLike() {
           fontWeight={"bold"}
           alignItems={"center"}
         >
-          좋아요한 게시글 목록
+          <Flex gap={4}>
+            <Box>
+              <FontAwesomeIcon
+                icon={faThumbsUp}
+                color={"orange"}
+                fontSize={"2.5rem"}
+              />
+            </Box>
+            <Box>좋아요한 게시글 목록</Box>
+          </Flex>
+        </CardHeader>
+
+        <CardHeader>
+          <Center mt={2} mb={4}>
+            <Flex justifyContent="space-between" w="100%">
+              <Flex gap={4}>
+                <Button {...buttonStyle} onClick={handleSelectAll}>
+                  전체 선택
+                </Button>
+                <Button {...buttonStyle} onClick={handleDesSelectAll}>
+                  전체 해제
+                </Button>
+              </Flex>
+
+              <Button
+                {...buttonStyle}
+                background={"orange"}
+                color={"black"}
+                onClick={handleSelectDeleteClick}
+              >
+                선택 삭제
+              </Button>
+            </Flex>
+          </Center>
         </CardHeader>
 
         <CardBody>
@@ -109,11 +189,38 @@ export function MemberBoardLike() {
               {gameBoardList.map((gameBoard) => (
                 <Tr key={gameBoard.id}>
                   <Td textAlign={"center"}>
-                    <Checkbox size={"lg"} colorScheme={"orange"}></Checkbox>
+                    <Checkbox
+                      size={"lg"}
+                      colorScheme={"orange"}
+                      isChecked={checkLikeGameBoard.includes(gameBoard.id)}
+                      onChange={(e) => handleLikeRowChange(e, gameBoard.id)}
+                    ></Checkbox>
                   </Td>
-                  <Td textAlign={"center"}>{gameBoard.category}</Td>
-                  <Td textAlign={"center"}>{gameBoard.title}</Td>
-                  <Td textAlign={"center"}>{gameBoard.board_content}</Td>
+                  <Td
+                    textAlign={"center"}
+                    _hover={{ cursor: "pointer" }}
+                    onClick={() => navigate("/gameboard/id/" + gameBoard.id)}
+                  >
+                    {gameBoard.category}
+                  </Td>
+                  <Td
+                    textAlign={"center"}
+                    _hover={{ cursor: "pointer" }}
+                    onClick={() => navigate("/gameboard/id/" + gameBoard.id)}
+                  >
+                    {gameBoard.title.length > 10
+                      ? gameBoard.title.slice(0, 10) + "..."
+                      : gameBoard.title}
+                  </Td>
+                  <Td
+                    textAlign={"center"}
+                    _hover={{ cursor: "pointer" }}
+                    onClick={() => navigate("/gameboard/id/" + gameBoard.id)}
+                  >
+                    {gameBoard.board_content.length > 15
+                      ? gameBoard.board_content.slice(0, 15) + "..."
+                      : gameBoard.board_content}
+                  </Td>
                   <Td textAlign={"center"} _hover={{ cursor: "pointer" }}>
                     <FontAwesomeIcon
                       icon={faXmark}
@@ -126,14 +233,6 @@ export function MemberBoardLike() {
               ))}
             </Tbody>
           </Table>
-          <Center mt={16} mb={20}>
-            <Flex gap={10}>
-              <Button {...buttonStyle}>전체 선택</Button>
-              <Button {...buttonStyle} background={"orange"} color={"black"}>
-                선택 삭제
-              </Button>
-            </Flex>
-          </Center>
         </CardBody>
       </Card>
     </Center>
