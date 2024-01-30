882adb0ea954be646bb8183c5e30595fd0282f5c
smil98 <seungmilee98@gmail.com>
2024-01-30 18:24:47 +0900

    카트 선택 결제 후 카트에서 삭제하는 로직 추가, 상품 결제 완료 페이지 CSS 수정 완료

--- src/page/order/SuccessPage.js
+++ src/page/order/SuccessPage.js
@@ -1,6 +1,16 @@
 import React, { useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import axios from "axios";
+import {
+  Box,
+  Card,
+  CardBody,
+  CardHeader,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
+import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const apiSecretKey = process.env.REACT_APP_TOSS_SECRET_KEY;
 
@@ -10,7 +20,7 @@ function SuccessPage() {
 
   // 수량, 고객번호, 고유 나노아이디, 주문명, 고객명
 
-  ///payment?paymentType={PAYMENT_TYPE}&orderId={ORDER_ID}&paymentKey={PAYMENT_KEY}&amount={AMOUNT}
+  ///success?paymentType={PAYMENT_TYPE}&orderId={ORDER_ID}&paymentKey={PAYMENT_KEY}&amount={AMOUNT}
 
   useEffect(() => {
     const requestData = {
@@ -45,16 +55,41 @@ function SuccessPage() {
   }, []);
 
   return (
-    <div className="result wrapper">
-      <div className="box_section">
-        <h2>결제 성공</h2>
-        <p>{`주문번호: ${searchParams.get("orderId")}`}</p>
-        <p>{`결제 금액: ${Number(
-          searchParams.get("amount"),
-        ).toLocaleString()}원`}</p>
-        <p>{`paymentKey: ${searchParams.get("paymentKey")}`}</p>
-      </div>
-    </div>
+    <Card mx="35%" my="5%" shadow="md">
+      <CardHeader textAlign="center">
+        <Text color="#6FA7DD" fontSize="6xl">
+          <FontAwesomeIcon icon={faCircleCheck} />
+        </Text>
+        <Heading>결제 되었습니다</Heading>
+      </CardHeader>
+      <CardBody my={5}>
+        <Box bgColor="#E6F0FC" color="#086DDD" p={3} borderRadius={10}>
+          <Text fontSize="sm">
+            <Text as="span" fontWeight="bold">
+              주문 번호
+            </Text>{" "}
+            `${searchParams.get("orderId")}`
+          </Text>
+          <Text fontSize="sm">
+            <Text as="span" fontWeight="bold">
+              결제 금액
+            </Text>{" "}
+            `${Number(searchParams.get("amount").toLocaleString())}원`
+          </Text>
+        </Box>
+      </CardBody>
+
+      {/*<div className="result wrapper">*/}
+      {/*  <div className="box_section">*/}
+      {/*    <h2>결제 성공</h2>*/}
+      {/*    <p>{`주문번호: ${searchParams.get("orderId")}`}</p>*/}
+      {/*    <p>{`결제 금액: ${Number(*/}
+      {/*      searchParams.get("amount"),*/}
+      {/*    ).toLocaleString()}원`}</p>*/}
+      {/*    <p>{`paymentKey: ${searchParams.get("paymentKey")}`}</p>*/}
+      {/*  </div>*/}
+      {/*</div>*/}
+    </Card>
   );
 }
 
